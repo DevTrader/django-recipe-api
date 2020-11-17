0be91c3f4d53f446dacae5cce63b24bad989d0d2
nicolas <nkaosartor@gmail.com>
2020-11-16 17:12:26 -0800

    added basic test to test travis

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 docker-compose build </br>
 docker-compose run app sh -c "" </br>
+
+## To run tests:
+
+docker-compose run app sh -c "python manage.py test" </br>
---
+++ app/app/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from app.calc import add 
+
+class CalcTests(TestCase):
+    """Define: calc module tests"""
+    def test_add_numbers(self):
+        """It: should add two numbers"""
+        self.assertEqual(add(5,3), 8)
