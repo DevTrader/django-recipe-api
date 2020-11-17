bff24516bcacd654a10d287fadf382d9c1347e7c
nicolas <nkaosartor@gmail.com>
2020-11-16 17:26:14 -0800

    fixed linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
-from app.calc import add 
+from app.calc import add
+
 
 class CalcTests(TestCase):
     """Define: calc module tests"""
     def test_add_numbers(self):
         """It: should add two numbers"""
-        self.assertEqual(add(5,3), 8)
+        self.assertEqual(add(5, 3), 8)
+        
