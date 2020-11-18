43a4ef4b6df4976694349f77a0e3a4e4a17ef7a0
nicolas <nkaosartor@gmail.com>
2020-11-17 17:24:50 -0800

    added user model and migrations

--- app/app/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from app.calc import add
-
-
-class CalcTests(TestCase):
-    """Define: calc module tests"""
-    def test_add_numbers(self):
-        """It: should add two numbers"""
-        self.assertEqual(add(5, 3), 8)
